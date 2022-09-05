6d15cd71b7e2177cd30b6dc08978455f4372a754
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-09-05 11:59:24 -0700

    Updating build container path

--- bin/build-container.sh
+++ bin/build-container.sh
@@ -4,7 +4,7 @@ pushd "$(git rev-parse --show-toplevel)"
 
 source bin/lib.sh
 
-docker build --build-arg version=${version} --tag ${container}:${tag} datetime-display
+docker build --build-arg version=${version} --tag ${container}:${tag} .
 docker tag ${container}:${tag} ${container}:${version}
 
 popd > /dev/null
