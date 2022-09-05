d2af56bc1b0747ed208cc5af6016d9a218410e96
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-09-05 11:11:19 -0700

    Re-order header nav links

--- src/main.rs
+++ src/main.rs
@@ -21,28 +21,3 @@ fn rocket() -> _ {
 fn not_found(req: &Request) -> String {
     format!("Oh no! We couldn't find the requested path '{}'", req.uri())
 }
-
-/*
-TODO
-
-Unix Epoch
-    To Date
-    From Date
-Time Zone
-    Convert to Different Time Zone
-    Show Local Time
-    Show UTC
-Formatting
-    Cheat Sheet of Format Codes
-    Format Preview/Builder
-    Convert Between Formats
-    Parse Format From Date
-    Examples
-Date Diff
-Human Readable 
-
-Links
-
-https://devhints.io/datetime
-
-*/
