79e43c0d2926b7def55918e27a0a3803f10a9646
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-09-05 17:07:02 -0700

    Display local and utc on home page

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,13 +1,31 @@
-use rocket::http::uri::Origin;
+use chrono::{Local};
+
+// use chrono::offset::{Local, Utc};
+use rocket::{http::uri::Origin};
 use rocket_dyn_templates::{context, Template};
 
 #[get("/")]
 pub fn index(origin: &Origin) -> Template {
+    let localdatetime = Local::now();
+    let localdate = localdatetime.format("%-m/%-d/%Y").to_string();
+    let localtime = localdatetime.format("%-I:%M:%S %p").to_string();
+    let localtimezone = localdatetime.format("%Z").to_string();
+
+    let utcdatetime = localdatetime.naive_utc();
+    let utcdate = utcdatetime.format("%-m/%-d/%Y").to_string();
+    let utctime = utcdatetime.format("%-I:%M:%S %p").to_string();
+    let utctimezone = "00:00";
+    
     Template::render(
         "home/index",
         context! {
-            field: "value",
-            origin: origin
-        },
+            origin: origin,
+            localdate: localdate,
+            localtime: localtime,
+            localtimezone: localtimezone,
+            utcdate: utcdate,
+            utctime: utctime,
+            utctimezone: utctimezone
+        }
     )
 }
